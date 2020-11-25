d0831735a2fca459094b638f5332f103499009a2
Kihyun.Hwang <stork838@gmail.com>
2020-11-26 07:40:57 +0900

    cors 설정 추가

--- src/main/java/com/keichee/BlogApp.java
+++ src/main/java/com/keichee/BlogApp.java
@@ -2,10 +2,24 @@ package com.keichee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = {"com.keichee"})
 public class BlogApp {
     public static void main(String[] args) {
         SpringApplication.run(BlogApp.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
